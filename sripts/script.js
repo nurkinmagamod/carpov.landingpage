f9e039ab1fd0978311e2981b49c6b39b9b565eb4
nurkinmagamod <battchat@gmail.com>
2020-07-17 15:47:50 +0700

    new repo

---
+++ sripts/script.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-icon-wrapper').onclick = function () {
+  document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
+  document.querySelector('.header__menu').classList.toggle('active');
+};
