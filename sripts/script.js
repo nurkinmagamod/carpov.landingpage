f39e3b4b2c6476170853fe3207700ecdd19c2a98
nurkinmagamod <battchat@gmail.com>
2020-07-22 17:57:46 +0700

    Fixes in mobile version. Added tab version of services

--- sripts/script.js
+++ sripts/script.js
@@ -1,4 +1,21 @@
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
+
 document.querySelector('.menu-icon-wrapper').onclick = function () {
   document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
   document.querySelector('.header__menu').classList.toggle('active');
 };
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove('active');
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove('active');
+    });
+    tab.classList.add('active');
+    target.classList.add('active');
+  });
+});
