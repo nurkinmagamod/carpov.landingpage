47158c3eaa6a2d8cc44eb04de7d58fa94563408c
nurkinmagamod@git config --global user.name nurkinmagamodgit config --global user.name nurkinmagamod@ <battchat@gmail.com>
2020-07-29 23:40:41 +0700

    Added smooth scroll

--- sripts/script.js
+++ sripts/script.js
@@ -1,21 +1,38 @@
-const tabs = document.querySelectorAll('[data-tab-target]');
-const tabContents = document.querySelectorAll('[data-tab-content]');
+const tabs = document.querySelectorAll("[data-tab-target]");
+const tabContents = document.querySelectorAll("[data-tab-content]");
 
-document.querySelector('.menu-icon-wrapper').onclick = function () {
-  document.querySelector('.menu-icon').classList.toggle('menu-icon-active');
-  document.querySelector('.header__menu').classList.toggle('active');
+document.querySelector(".menu-icon-wrapper").onclick = function () {
+  document.querySelector(".menu-icon").classList.toggle("menu-icon-active");
+  document.querySelector(".header__menu").classList.toggle("active");
 };
 
 tabs.forEach((tab) => {
-  tab.addEventListener('click', () => {
+  tab.addEventListener("click", () => {
     const target = document.querySelector(tab.dataset.tabTarget);
     tabContents.forEach((tabContent) => {
-      tabContent.classList.remove('active');
+      tabContent.classList.remove("active");
     });
     tabs.forEach((tab) => {
-      tab.classList.remove('active');
+      tab.classList.remove("active");
     });
-    tab.classList.add('active');
-    target.classList.add('active');
+    tab.classList.add("active");
+    target.classList.add("active");
+  });
+});
+
+document.querySelectorAll('a[href^="#"').forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    let href = this.getAttribute("href").substring(1);
+    const scrollTarget = document.getElementById(href);
+    const topOffset = document.querySelector(".header__menu").offsetHeight;
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+    const offsetPosition = elementPosition - topOffset;
+
+    window.scrollBy({
+      top: offsetPosition,
+      behavior: "smooth",
+    });
   });
 });
